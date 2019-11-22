2075ef618f585f60f8cd15365ce92b692c7e7763
Trash Panda <d.george.schlosser@gmail.com>
2019-11-21 19:55:54 -0500

    halve size of 'stars'

--- js/stars.js
+++ js/stars.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     this.obj.classList.add("box");
     this.obj.style.top = (window.innerHeight * Math.random()) + 'px';
     this.obj.style.left = (window.innerWidth * Math.random()) + 'px';
-    this.size = Math.floor(1 * Math.random()) + 7;
+    this.size = Math.floor(1 * Math.random()) + 3.5;
     this.obj.style.height =  this.size + 'px';
     this.obj.style.width = this.size + 'px';
     
@@ -15,11 +15,11 @@ $(document).ready(function(){
   for(var i = 0 ; i < 200 ; i++ ){
     dot.push(new freshDot());
   }
-  /*
-  $(window).resize(function(){
-    for(i=0;i<200;i++){
-      document.body.removeChild(dot[i]);
-    }
-  });
-  */
+  
+  // $(window).resize(function(){
+  //   for(i=0;i<200;i++){
+  //     document.body.removeChild(dot[i]);
+  //   }
+  // });
+  
 });
